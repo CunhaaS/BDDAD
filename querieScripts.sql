5d9d4ee19dcfee4ac8b309d52e4fcdd94d6eaa86
José Cunha <zezecunha4@gmail.com>
2018-10-13 12:20:31 +0100

    Done alguns

--- querieScripts.sql
+++ querieScripts.sql
@@ -1,6 +1,11 @@
 --a)
-
-
+select avg(E.salario_semanal), (E.salario_semanal * 48) as Salario_Anual 
+from empregado E
+where E.cod_armazem = (
+        select A.cod_armazem 
+        from armazem A
+        where A.nome like 'Parafusos'
+);
 
 --b)
 
@@ -38,8 +43,24 @@ WHERE cod_produto IN (SELECT cod_produto
                                                                            GROUP BY cod_armazem)));   
 --d)
 
-
-
+with data_estado as (
+select A.cod_armazem, A.nome, A.cidade, OC.data_entrega,OC.estado from armazem A
+INNER JOIN Empregado E ON E.cod_armazem = A.cod_armazem
+INNER JOIN OrdemCompra OC ON OC.cod_empregado = E.cod_empregado
+where (OC.data_entrega BETWEEN '01/03/2018' AND '15/10/2018'))
+select DE.cod_armazem from data_estado DE
+Group by DE.cod_armazem
+HAVING
+    COUNT(*) = (
+        SELECT
+            MAX(COUNT(*) )
+        FROM
+            data_estado DE1
+        WHERE
+            DE1.estado = 2 and DE1.cidade Like 'Porto'
+        GROUP BY
+            DE1.cod_armazem
+    )
 
 --e)
 
