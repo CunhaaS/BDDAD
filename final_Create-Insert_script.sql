c4a83c685e72d2a9e838cea0f81a9c011991e0f6
João Zarco <joao.zarco@gmail.com>
2018-10-13 12:59:37 +0100

    update tables to get time

--- final_Create-Insert_script.sql
+++ final_Create-Insert_script.sql
@@ -71,7 +71,7 @@ CREATE TABLE ordem_compra (
     nr_ordem        INTEGER         CONSTRAINT pk_ordem_compra_produto_nr_ordem PRIMARY KEY,
     cod_fornecedor  INTEGER         REFERENCES fornecedor(cod_fornecedor),
     cod_empregado   INTEGER         REFERENCES empregado(cod_empregado),
-    data_compra     DATE            CONSTRAINT nn_ordem_compra_data_compra      NOT NULL,
+    data_compra     DATE           CONSTRAINT nn_ordem_compra_data_compra      NOT NULL,
     valor_total     NUMERIC(10,2)   CONSTRAINT nn_ordem_compra_valor_total      NOT NULL,
     data_entrega    DATE            CONSTRAINT nn_ordem_compra_data_entrega     NOT NULL,
     estado          INTEGER         CONSTRAINT nn_ordem_compra_estado           NOT NULL
@@ -158,18 +158,18 @@ INSERT INTO empregado VALUES(9,8,3,'Filipe Silva','52 Rock Maple Circle Oak Lawn
 INSERT INTO empregado VALUES(10,8,3,'Maria Leal','Rua das flores 456512, IF 31343',786,'Canalização');
 
 --ordens de compra
-INSERT INTO ordem_compra VALUES(1,1,1,TO_TIMESTAMP('11-08-2017 10:20','dd-mm-yyyy hh24:mi'),1500,TO_TIMESTAMP('18-08-2017 18:29','dd-mm-yyyy hh24:mi'),1);
-INSERT INTO ordem_compra VALUES(2,1,2,TO_TIMESTAMP('27-10-2016 13:40','dd-mm-yyyy hh24:mi'),330,TO_TIMESTAMP('30-10-2016 13:10','dd-mm-yyyy hh24:mi'),2);
-INSERT INTO ordem_compra VALUES(3,2,3,TO_TIMESTAMP('14-11-2015 18:29','dd-mm-yyyy hh24:mi'),432,TO_TIMESTAMP('20-11-2015 9:20','dd-mm-yyyy hh24:mi'),2);
-INSERT INTO ordem_compra VALUES(4,2,4,TO_TIMESTAMP('28-09-2018 15:32','dd-mm-yyyy hh24:mi'),900,TO_TIMESTAMP('14-10-2018 8:40','dd-mm-yyyy hh24:mi'),3);
-INSERT INTO ordem_compra VALUES(5,3,5,TO_TIMESTAMP('30-03-2013 07:34','dd-mm-yyyy hh24:mi'),1302,TO_TIMESTAMP('10-04-2013 7:53','dd-mm-yyyy hh24:mi'),1);
-INSERT INTO ordem_compra VALUES(6,3,6,TO_TIMESTAMP('03-08-2012 09:54','dd-mm-yyyy hh24:mi'),2000,TO_TIMESTAMP('06-08-2012 18:00','dd-mm-yyyy hh24:mi'),2);
-INSERT INTO ordem_compra VALUES(7,4,7,TO_TIMESTAMP('04-12-2017 18:23','dd-mm-yyyy hh24:mi'),1567,TO_TIMESTAMP('11-12-2017 10:20','dd-mm-yyyy hh24:mi'),1);
-INSERT INTO ordem_compra VALUES(8,4,8,TO_TIMESTAMP('18-07-2018 12:21','dd-mm-yyyy hh24:mi'),520,TO_TIMESTAMP('25-07-2018 19:10','dd-mm-yyyy hh24:mi'),2);
-INSERT INTO ordem_compra VALUES(9,5,9,TO_TIMESTAMP('27-02-2016 20:30','dd-mm-yyyy hh24:mi'),420,TO_TIMESTAMP('04-03-2016 10:01','dd-mm-yyyy hh24:mi'),3);
-INSERT INTO ordem_compra VALUES(10,5,1,TO_TIMESTAMP('09-08-2015 16:20','dd-mm-yyyy hh24:mi'),1987,TO_TIMESTAMP('17-08-2015 13:59','dd-mm-yyyy hh24:mi'),1);
-INSERT INTO ordem_compra VALUES(11,6,2,TO_TIMESTAMP('23-05-2018 14:54','dd-mm-yyyy hh24:mi'),6789,TO_TIMESTAMP('30-05-2018 20:40','dd-mm-yyyy hh24:mi'),2);
-INSERT INTO ordem_compra VALUES(12,6,6,TO_TIMESTAMP('13-01-2018 14:10','dd-mm-yyyy hh24:mi'),126,TO_TIMESTAMP('20-01-2018 17:30','dd-mm-yyyy hh24:mi'),1);
+INSERT INTO ordem_compra VALUES(1,1,1,'11-08-2017 10:20','dd-mm-yyyy hh:mi',1500,'18-08-2017 18:29','dd-mm-yyyy hh:mi',1);
+INSERT INTO ordem_compra VALUES(2,1,2,'27-10-2016 13:40','dd-mm-yyyy hh:mi',330,'30-10-2016 13:10','dd-mm-yyyy hh:mi',2);
+INSERT INTO ordem_compra VALUES(3,2,3,'14-11-2015 18:29','dd-mm-yyyy hh:mi',432,'20-11-2015 9:20','dd-mm-yyyy hh:mi',2);
+INSERT INTO ordem_compra VALUES(4,2,4,'28-09-2018 15:32','dd-mm-yyyy hh:mi',900,'14-10-2018 8:40','dd-mm-yyyy hh:mi',3);
+INSERT INTO ordem_compra VALUES(5,3,5,'30-03-2013 07:34','dd-mm-yyyy hh:mi',1302,'10-04-2013 7:53','dd-mm-yyyy hh:mi',1);
+INSERT INTO ordem_compra VALUES(6,3,6,'03-08-2012 09:54','dd-mm-yyyy hh:mi',2000,'06-08-2012 18:00','dd-mm-yyyy hh:mi',2);
+INSERT INTO ordem_compra VALUES(7,4,7,'04-12-2017 18:23','dd-mm-yyyy hh:mi',1567,'11-12-2017 10:20','dd-mm-yyyy hh:mi',1);
+INSERT INTO ordem_compra VALUES(8,4,8,'18-07-2018 12:21','dd-mm-yyyy hh:mi',520,'25-07-2018 19:10','dd-mm-yyyy hh:mi',2);
+INSERT INTO ordem_compra VALUES(9,5,9,'27-02-2016 20:30','dd-mm-yyyy hh:mi',420,'04-03-2016 10:01','dd-mm-yyyy hh:mi',3);
+INSERT INTO ordem_compra VALUES(10,5,1,'09-08-2015 16:20','dd-mm-yyyy hh:mi',1987,'17-08-2015 13:59','dd-mm-yyyy hh:mi',1);
+INSERT INTO ordem_compra VALUES(11,6,2,'23-05-2018 14:54','dd-mm-yyyy hh:mi',6789,'30-05-2018 20:40','dd-mm-yyyy hh:mi',2);
+INSERT INTO ordem_compra VALUES(12,6,6,'13-01-2018 14:10','dd-mm-yyyy hh:mi',126,'20-01-2018 17:30','dd-mm-yyyy hh:mi',1);
 
 --ordens de compra de um produto
 INSERT INTO ordem_compra_produto VALUES(1,43,1,1700,20);
